ea2ed5aa2c0aa1daab56b8d7a4ee25c4267fc135
szKubica <264262@student.pwr.edu.pl>
2022-06-09 22:40:48 +0200

    commit>

--- kolo.py
+++ kolo.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+from itertools import islice
 
 
 start_date = '02.12.2021' #input("Input date of start ")
 start_hour = '10:15:00' #input("Input hour of start ")
 end_hour = '10:16:00' #input("Input hour of end ")
 start_date = start_date[:6]+start_date[8:] # converting 02.12.2021 to 02.21.21 format
-path = r"C:\Users\Lenovo\Kolo naukowe\Dane1.csv"
+path = r"C:\Users\Lenovo\Kolo naukowe\Dane.csv"
 
 def get_sec(time_str):
     h, m, s = time_str.split(':')
@@ -25,21 +26,50 @@ def grap_drawer(graph_end_row, graph_start_row, path):
     plt.show()
 
 
-line_number=0
-with open(path) as df:
-    f = csv.reader(df)
-    for row in f:
-        line_number += 1
-        if start_date in row and get_sec(row[6]) < get_sec(start_hour):
-            accurate_row = row
-            print(row, line_number)
-            i = line_number
-
-survey_start = get_sec(accurate_row[6])
-frequency = int(accurate_row[4])
-graph_start_row = i+(get_sec(start_hour)-get_sec(accurate_row[6]))*frequency
-graph_end_row = graph_start_row + (get_sec(end_hour)-get_sec(start_hour))*frequency
+def sample_intervals(max_value, operator, path, graph_start_row, graph_end_row):
+    sample = 0
+    samples_list = []
+    with open(path) as df:
+        f = csv.reader(df)
+        for row in islice(f, graph_start_row, graph_end_row):
+            if eval('int(row[3]) {} max_value'.format(operator)):
+                sample += 1
+            else:
+                if sample != 0:
+                    samples_list.append(sample)
+                    sample = 0
+    return samples_list
 
 
 if __name__ == "__main__":
+    max_value = 0
+    line_number = 0
+
+    with open(path) as df:
+        f = csv.reader(df)
+        for row in f:
+            line_number += 1
+            if start_date in row and get_sec(row[6]) < get_sec(start_hour):
+                accurate_row = row
+                i = line_number
+
+    frequency = int(accurate_row[4])
+    graph_start_row = i + (get_sec(start_hour) - get_sec(accurate_row[6])) * frequency
+    graph_end_row = graph_start_row + (get_sec(end_hour) - get_sec(start_hour)) * frequency
+
+    with open(path) as df:
+        f = csv.reader(df)
+        for row in islice(f, graph_start_row, graph_end_row):
+            if int(row[3]) > max_value:
+                max_value = int(row[3])
+    max_value = 0.2*max_value
+
+
+
+
+    print(sample_intervals(max_value, ">", path, graph_start_row, graph_end_row))
+    print(sample_intervals(max_value, "<", path, graph_start_row, graph_end_row))
     grap_drawer(graph_end_row, graph_start_row, path)
+
+
+
